e07621a88d09c1f5f3fc03c984782ba359baa5ee
foxsugar <redalert3@126.com>
2017-09-30 11:16:50 +0800

    依赖

--- setup.py
+++ setup.py
@@ -1,72 +1,42 @@
-from codecs import open  # To use a consistent encoding
-from os import path
-
 from setuptools import setup, find_packages
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
-    name='summer_admin',
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.1',
+    name="summer_admin",
+    version="1.0",
+    keywords=("test", "xxx"),
+    description="summer admin",
+    long_description="summer adminfor python",
+    license="MIT Licence",
 
-    description='summer_admin',
-    long_description=long_description,
+    url="url",
+    author="test",
+    author_email="test@gmail.com",
 
-
-
-
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: Other/Proprietary License',
-
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-    ],
-
-    keywords='summer_admin',
-
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    namespace_packages=['summer_admin'],
-
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
     install_requires=[
         'pymysql>=0.7.11',
         'django>=1.10.3',
         'django-cors-headers>=2.1.0',
         'thrift>=0.10.0'
-
     ],
 
-    include_package_data=True,
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={
-        # 'sample': ['package_data.dat'],
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[
-    ],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
+    scripts=[],
     entry_points={
         'console_scripts': [
-            # 'sample=sample:main',
-        ],
+            'test = test.help:main'
+        ]
     },
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    # 此项需要，否则卸载时报windows error
+    zip_safe=False
+
 )
