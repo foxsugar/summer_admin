7e41ff1309bb7cbabb7f744bd798e70419eee752
zbb <1067715297@qq.com>
2017-11-24 16:59:12 +0800

    更新标题

--- summer_admin/apps/officailWebViews.py
+++ summer_admin/apps/officailWebViews.py
@@ -6,11 +6,15 @@ from summer_admin.apps.views import *
 def officail_web(request):
     category = config.get('robot', 'gameCategory')
     url = None;
+    game_name = None
     if category == "tongcheng":
         url = "http://fir.im/ncdp"
+        game_name = "河北同城麻将"
     elif category == "laotie":
         url = "http://fir.im/ncjb"
+        game_name = "老铁棋牌"
     else:
         url = ""
-    dict = {"url": url, "category": category}
+        game_name = ""
+    dict = {"url": url, "category": category, "game_name" : game_name}
     return render(request, "officialWeb.html", {"data": dict})
