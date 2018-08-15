ba932e0f5874e1bf0f33897fd1755b9db6afca1e
zhaobb <zhaobb@rihuisoft.com>
2018-08-15 11:29:21 +0800

    a

--- summer_admin/apps/robotViews.py
+++ summer_admin/apps/robotViews.py
@@ -255,7 +255,7 @@ def get_majiang_room_rule(data):
         else:
             ss = "圈"
 
-        finalStr = "%s,%s,%s,%s,%倍%s%s" % (gameTypeName,personType,turn, ss,multiple,context,huangStr)
+        finalStr = "%s,%s,%s,%s,%s倍%s%s" % (gameTypeName,personType,turn, ss,multiple,context,huangStr)
         return  finalStr
     except:
         return ""
